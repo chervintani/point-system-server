1757cad36d4968881b2171ce93a7e4e32273eaa7
Chervin Tanilon <chervin.tanilon@student.passerellesnumeriques.org>
2020-11-13 23:02:18 +0800

    deactivated account

--- modules/user/user.login.js
+++ modules/user/user.login.js
@@ -1,30 +1,52 @@
-
 let Model = require("../../models/user");
 let errorResponse = require("../../helpers/error-response");
 let successResponse = require("../../helpers/success-response");
 let response = null;
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-module.exports = (req,res)=>{
-    Model.User.findOne({ username: req.username },{feeds_activity:0}).then((user) => {
-        if (!user) {
-          response = errorResponse(404,{exists: false}, "Account does not exists. Do you want to create one?")
-          res.status(response.status).send(response)
-        } else {
+module.exports = (req, res) => {
+  Model.User.findOne({ username: req.username }, { feeds_activity: 0 })
+    .then((user) => {
+      if (!user) {
+        response = errorResponse(
+          404,
+          { exists: false },
+          "Account does not exists. Do you want to create one?"
+        );
+        res.status(response.status).send(response);
+      } else {
+        if (user.status === "Activated") {
           bcrypt.compare(req.password, user.password).then((isMatch) => {
             if (isMatch) {
-                let payload = {subject: user._id}
-                let token = jwt.sign(payload,'secretKey')
-                response = successResponse(200,{token: token,user:user},"Password matched")
-                res.status(response.status).send(response);
+              let payload = { subject: user._id };
+              let token = jwt.sign(payload, "secretKey");
+              response = successResponse(
+                200,
+                { token: token, user: user },
+                "Password matched"
+              );
+              res.status(response.status).send(response);
             } else {
-              response = errorResponse(401,{exists: true}, "Incorrect password!")
-              res.status(response.status).send(response)
+              response = errorResponse(
+                401,
+                { exists: true },
+                "Incorrect password!"
+              );
+              res.status(response.status).send(response);
             }
           });
+        } else {
+          response = errorResponse(
+            403,
+            { success: false },
+            "Sorry, this account has been deactivated by the administrator due to some reasons."
+          );
+          res.status(response.status).send(response);
         }
-      }).catch((err) => {
-        response = errorResponse(503, err, "Service Unavailable!");
-        res.status(response.status).send(response);
-      });
-}
+      }
+    })
+    .catch((err) => {
+      response = errorResponse(503, err, "Service Unavailable!");
+      res.status(response.status).send(response);
+    });
+};
